8607c755783166f394e10209ba187bfbc2e75ff4
Gabriel Resende <gabriel@sensio.com.br>
2024-06-04 08:39:11 -0300

    feat: Add types for developers and levels

---
+++ backend/src/types/types.ts
@@ -0,0 +1,16 @@
+export interface IDesenvolvedor {
+  id?: number;
+  nivel_id: number;
+  nome: string;
+  sexo: string;
+  data_nascimento: Date;
+  idade: number;
+  hobby: string;
+  nivel?: INivel;
+}
+
+export interface INivel {
+  id?: number;
+  nivel: string;
+  Desenvolvedor?: IDesenvolvedor[];
+}

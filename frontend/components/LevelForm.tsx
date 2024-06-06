8d10a9b7c0abe31026d312807940aef6f0042514
Gabriel Resende <gabriel@sensio.com.br>
2024-06-06 13:43:20 -0300

    feat: set LevelForm component

--- frontend/components/LevelForm.tsx
+++ frontend/components/LevelForm.tsx
@@ -6,8 +6,7 @@ import { z } from "zod";
 import { toast } from "sonner";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { CalendarIcon, Edit, PlusCircle } from "lucide-react";
-import { format } from "date-fns";
+import { Edit, PlusCircle } from "lucide-react";
 
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
@@ -20,79 +19,37 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog";
-import { IDesenvolvedor, INivel } from "@/utils/types";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./ui/select";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
-import { cn } from "@/lib/utils";
-import { Calendar } from "./ui/calendar";
-import { Textarea } from "./ui/textarea";
+import { INivel } from "@/utils/types";
 
 const formSchema = z.object({
   id: z.number().optional().nullable(),
-  nome: z.string().min(3, {
-    message: "Nome deve ter no mínimo 3 caracteres.",
-  }),
-
-  sexo: z.string().min(1, {
-    message: "Sexo deve ser preenchido.",
-  }),
-  data_nascimento: z.date().refine(
-    (data) => {
-      const now = new Date();
-      const birth = new Date(data);
-
-      if (birth > now) {
-        return false;
-      }
-
-      return true;
-    },
-    { message: "Data de nascimento deve ser menor que a data atual." }
-  ),
-
-  idade: z.number().min(1, {
-    message: "Idade deve ser preenchida.",
-  }),
-  hobby: z.string().min(3, {
-    message: "Hobby deve ter no mínimo 3 caracteres.",
-  }),
-  nivel_id: z.number().min(1, {
-    message: "Nível deve ser preenchido.",
+  nivel: z.string().min(3, {
+    message: "Nivel deve ter no mínimo 3 caracteres.",
   }),
 });
 
-export function DevForm({ desenvolvedor }: { desenvolvedor?: IDesenvolvedor }) {
+export function LevelForm({ nivel }: { nivel?: INivel }) {
   const [loading, setLoading] = useState(false);
-  const [niveis, setNiveis] = useState<INivel[]>([]);
 
   const dialogRef = useRef<HTMLButtonElement>(null);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: desenvolvedor,
+    defaultValues: {
+      id: null,
+      nivel: "",
+    },
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const { id, nome, sexo, data_nascimento, idade, hobby, nivel_id } =
-        values as IDesenvolvedor;
+      const { id, nivel } = values as INivel;
 
       setLoading(true);
 
-      const data: IDesenvolvedor = {
+      const data: INivel = {
         id,
-        nome,
-        sexo,
-        data_nascimento: new Date(data_nascimento),
-        idade,
-        hobby,
-        nivel_id,
+        nivel,
       };
 
       console.log(data);
@@ -100,147 +57,52 @@ export function DevForm({ desenvolvedor }: { desenvolvedor?: IDesenvolvedor }) {
       dialogRef.current?.click();
       form.reset();
 
-      toast.success("Novo desenvolvedor cadastrado!");
+      toast.success("Novo nível cadastrado com sucesso.");
     } catch (error) {
-      toast.error("Erro ao cadastrar novo desenvolvedor.");
+      toast.error("Erro ao cadastrar novo nível.");
     } finally {
       setLoading(false);
     }
   };
 
+  const handleEdit = (nivel: INivel) => () => {
+    form.reset(nivel);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-        {!desenvolvedor?.id ? (
+        {!nivel?.id ? (
           <Button ref={dialogRef}>
             <PlusCircle className='w-4 h-4 mr-2' />
             Adicionar
           </Button>
         ) : (
-          <Button variant='secondary' ref={dialogRef}>
+          <Button
+            variant='secondary'
+            ref={dialogRef}
+            onClick={handleEdit(nivel)}
+          >
             <Edit className='w-4 h-4' />
           </Button>
         )}
       </DialogTrigger>
       <DialogContent>
-        <DialogTitle>Formulário de Desenvolvedor</DialogTitle>
+        <DialogTitle>
+          {nivel?.id ? "Editar Nível" : "Adicionar Nível"}
+        </DialogTitle>
 
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-5'>
             <FormField
               disabled={loading}
               control={form.control}
-              name='nome'
+              name='nivel'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Nome</FormLabel>
                   <FormControl>
-                    <Input placeholder='Nome' {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-
-            <div className='md:flex w-full md:space-x-2 max-sm:space-y-5'>
-              <FormField
-                disabled={loading}
-                control={form.control}
-                name='sexo'
-                render={({ field }) => (
-                  <FormItem className='flex-1'>
-                    <FormLabel>Sexo</FormLabel>
-                    <FormControl>
-                      <Select>
-                        <SelectTrigger>
-                          <SelectValue placeholder='Selecione seu sexo' />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectItem value='M'>Masculino</SelectItem>
-                          <SelectItem value='F'>Feminino</SelectItem>
-                        </SelectContent>
-                      </Select>
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                disabled={loading}
-                control={form.control}
-                name='data_nascimento'
-                render={({ field }) => (
-                  <FormItem className='flex-1'>
-                    <FormLabel>Data de Nascimento</FormLabel>
-                    <FormControl>
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <Button
-                            variant={"outline"}
-                            className={cn(
-                              "w-full justify-start text-left font-normal",
-                              !field.value && "text-muted-foreground"
-                            )}
-                          >
-                            <CalendarIcon className='mr-2 h-4 w-4' />
-                            {field.value ? (
-                              format(field.value, "dd/MM/yyyy")
-                            ) : (
-                              <span>Clique para selecionar</span>
-                            )}
-                          </Button>
-                        </PopoverTrigger>
-                        <PopoverContent className='w-auto p-0'>
-                          <Calendar
-                            mode='single'
-                            selected={field.value}
-                            onSelect={field.onChange}
-                            initialFocus
-                          />
-                        </PopoverContent>
-                      </Popover>
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <FormField
-              disabled={loading}
-              control={form.control}
-              name='nivel_id'
-              render={({ field }) => (
-                <FormItem className='flex-1'>
-                  <FormLabel>Nível</FormLabel>
-                  <FormControl>
-                    <Select>
-                      <SelectTrigger>
-                        <SelectValue placeholder='Selecione seu nível' />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {niveis?.map((nivel) => (
-                          <SelectItem key={nivel.id} value={String(nivel.id)}>
-                            {nivel.nivel}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              disabled={loading}
-              control={form.control}
-              name='hobby'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Hobbies</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder='Conte-nos um pouco sobre seus hobbies'
-                      {...field}
-                    />
+                    <Input placeholder='Nivel' {...field} />
                   </FormControl>
                 </FormItem>
               )}

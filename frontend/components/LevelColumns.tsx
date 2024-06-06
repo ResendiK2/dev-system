c1ff96eb454972d595f06cc2f4d76b82c1fa2be5
Gabriel Resende <gabriel@sensio.com.br>
2024-06-06 00:36:59 -0300

    feat: Add LevelColumns component for displaying level columns in a table

---
+++ frontend/components/LevelColumns.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { ArrowUpDown, Trash } from "lucide-react";
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Button } from "./ui/button";
+import { DeleteConfirmation } from "./DeleteConfirmation";
+
+import { INivel } from "@/utils/types";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+
+export const columns: ColumnDef<INivel>[] = [
+  {
+    accessorKey: "nivel",
+    header: ({ column }) => {
+      return (
+        <div className='flex flex-1 justify-center items-center w-full'>
+          <Button
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Nível
+            <ArrowUpDown className='ml-2 h-4 w-4' />
+          </Button>
+        </div>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("nivel")}</div>,
+  },
+  {
+    accessorKey: "n_desenvolvedores",
+    header: () => {
+      return (
+        <div className='flex justify-center items-center w-full'>
+          <Button variant='ghost'>Desenvolvedores vinculados</Button>
+        </div>
+      );
+    },
+    cell: ({ row }) => (
+      <div className='text-center'>
+        {row.getValue("n_desenvolvedores") || 0}
+      </div>
+    ),
+  },
+  {
+    id: "actions",
+    header: () => {
+      return (
+        <div className='flex justify-center items-center w-full'>
+          <Button variant='ghost'>Ações</Button>
+        </div>
+      );
+    },
+    cell: ({ row }) => {
+      const nivel = row.original as INivel;
+
+      return (
+        <div className='flex justify-center items-center space-x-2'>
+          {nivel.n_desenvolvedores === 0 ? (
+            <DeleteConfirmation id={+row.id} type='nivel' />
+          ) : (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <div>
+                    <Button variant='destructive' disabled>
+                      <Trash className='w-4 h-4' />
+                    </Button>
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>
+                    Não é possível excluir níveis com desenvolvedores
+                    vinculados.
+                  </p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
+        </div>
+      );
+    },
+  },
+];

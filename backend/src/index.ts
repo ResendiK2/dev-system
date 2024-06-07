fb325ef0a30f6358eb6a29b6122e0e17925b67e2
Gabriel Resende <gabriel@sensio.com.br>
2024-06-06 23:28:58 -0300

    refactor: Update backend to include Swagger documentation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
-import express from "express";
-import routes from "./routes";
-
 import * as dotenv from "dotenv";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+
+import routes from "./routes";
+import swaggerDocs from "./swagger.json";
 
 dotenv.config();
 
@@ -10,6 +12,7 @@ const port = process.env.PORT || 3333;
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use("/api", routes);
 
 app.listen(port, () => {

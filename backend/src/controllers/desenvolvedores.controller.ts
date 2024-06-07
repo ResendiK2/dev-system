d3bc7833b7b8e1eac766b94941a1005b553e47c7
Gabriel Resende <gabriel@sensio.com.br>
2024-06-06 23:43:54 -0300

    refactor: Implement pagination and search functionality in getDesenvolvedores controller

--- backend/src/controllers/desenvolvedores.controller.ts
+++ backend/src/controllers/desenvolvedores.controller.ts
@@ -9,14 +9,34 @@ import { IDesenvolvedor } from "../types/types";
 
 export const getDesenvolvedores = async (req: Request, res: Response) => {
   try {
-    const desenvolvedores = await getDesenvolvedoresService();
+    const { page = 1, per_page = 10, nome } = req.query;
 
-    if (!desenvolvedores?.length)
+    const paginaAtual = parseInt(page as string) || 1;
+    const porPagina = parseInt(per_page as string) || 10;
+
+    const skip = (paginaAtual - 1) * porPagina;
+    const take = porPagina;
+
+    const { desenvolvedores, total } = await getDesenvolvedoresService(
+      nome as string,
+      skip,
+      take
+    );
+
+    if (!desenvolvedores.length)
       return res
         .status(404)
         .json({ error: "Nenhum desenvolvedor encontrado." });
 
-    res.status(200).json(desenvolvedores);
+    res.status(200).json({
+      data: desenvolvedores,
+      meta: {
+        total,
+        per_page: porPagina,
+        current_page: paginaAtual,
+        last_page: Math.ceil(total / porPagina),
+      },
+    });
   } catch (error) {
     res.status(500).json({ error: "Erro ao buscar desenvolvedores." });
   }

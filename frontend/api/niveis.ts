6014b48adda4e79e30da2af2926f4da84ef437c0
Gabriel Resende <gabriel@sensio.com.br>
2024-06-07 19:10:00 -0300

    refactor: Update niveis API functions for CRUD operations and improve data fetching with pagination

---
+++ frontend/api/niveis.ts
@@ -0,0 +1,39 @@
+import { INivel } from "@/utils/types";
+import axios from "axios";
+
+const API_URL = "http://localhost:3333/api/niveis";
+
+export const getLevels = async ({
+  page = 1,
+  query = "",
+}: {
+  page?: number;
+  query?: string;
+}) => {
+  const response = await axios.get(`${API_URL}`, {
+    params: {
+      page,
+      query,
+    },
+  });
+
+  return response?.data || [];
+};
+
+export const createLevel = async (level: INivel) => {
+  const response = await axios.post(API_URL, level);
+
+  return response.data;
+};
+
+export const updateLevel = async (nivel: INivel) => {
+  const response = await axios.put(`${API_URL}/${nivel.id}`, nivel);
+
+  return response.data;
+};
+
+export const deleteLevel = async (id: number) => {
+  const response = await axios.delete(`${API_URL}/${id}`);
+
+  return response.data;
+};

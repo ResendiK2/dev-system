d46cdb4060c330f16a40cfbb5d43b7b3aa5e9e2c
Gabriel Resende <gabriel@sensio.com.br>
2024-06-06 23:28:43 -0300

    refactor: Update niveis.routes.ts to include CRUD operations for levels

--- backend/src/routes/niveis.routes.ts
+++ backend/src/routes/niveis.routes.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import {
+  createNivel,
+  deleteNivel,
+  getNiveis,
+  updateNivel,
+} from "../controllers/niveis.controller";
 
 const router = Router();
+
+router.get("/niveis", getNiveis);
+router.post("/niveis", createNivel);
+router.put("/niveis/:id", updateNivel);
+router.delete("/niveis/:id", deleteNivel);
 
 export default router;

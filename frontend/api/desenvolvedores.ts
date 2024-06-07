4fc452a15d2d6fd960dc4a6c3094f7ee0abbe9d0
Gabriel Resende <gabriel@sensio.com.br>
2024-06-07 19:10:12 -0300

    refactor: Update API functions for CRUD operations on developers and improve data fetching with pagination

--- frontend/api/desenvolvedores.ts
+++ frontend/api/desenvolvedores.ts
@@ -1,25 +1,37 @@
 // api/desenvolvedores.ts
+import { IDesenvolvedor } from "@/utils/types";
 import axios from "axios";
 
 const API_URL = "http://localhost:3333/api/desenvolvedores";
 
-export const getDevs = async (page: number, limit: number) => {
-  const response = await axios.get(`${API_URL}`);
+export const getDevs = async (page: number, query: string) => {
+  const response = await axios.get(`${API_URL}`, {
+    params: {
+      page,
+      query,
+    },
+  });
 
   return response?.data || [];
 };
 
-export const createDev = async (dev: any) => {
-  const response = await axios.post(API_URL, dev);
+export const createDev = async (desenvolvedor: IDesenvolvedor) => {
+  const response = await axios.post(API_URL, desenvolvedor);
+
   return response.data;
 };
 
-export const updateDev = async (id: string, dev: any) => {
-  const response = await axios.put(`${API_URL}/${id}`, dev);
+export const updateDev = async (desenvolvedor: IDesenvolvedor) => {
+  const response = await axios.put(
+    `${API_URL}/${desenvolvedor.id}`,
+    desenvolvedor
+  );
+
   return response.data;
 };
 
-export const deleteDev = async (id: string) => {
+export const deleteDev = async (id: number) => {
   const response = await axios.delete(`${API_URL}/${id}`);
+
   return response.data;
 };

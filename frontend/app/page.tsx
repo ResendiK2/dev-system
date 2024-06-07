9ba358518fec9b01cc75674e8a66662e51495d82
Gabriel Resende <gabriel@sensio.com.br>
2024-06-07 19:09:30 -0300

    refactor: Update Desenvolvedores component to use API functions for CRUD operations and improve data fetching with pagination

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { toast } from "sonner";
 import { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 
 import {
   Breadcrumb,
@@ -17,26 +17,60 @@ import { TableComponent } from "@/components/DataTable";
 import { Toaster } from "../components/ui/sonner";
 
 import { getDevs } from "@/api/desenvolvedores";
-import { ICustomError } from "@/utils/types";
+import {
+  ICustomError,
+  IDesenvolvedor,
+  IGetDesenvolvedores,
+} from "@/utils/types";
 
 export default function Desenvolvedores() {
   const [page, setPage] = useState(1);
-  const [limit] = useState(10);
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState<IGetDesenvolvedores>(
+    {} as IGetDesenvolvedores
+  );
+  const [isLoading, setIsLoading] = useState(true);
 
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["devs", page, limit],
-    queryFn: () => getDevs(page, limit),
-  });
+  // const { data, error, isLoading } = useQuery({
+  //   queryKey: ["devs", page],
+  //   queryFn: () => getDevs(page, query),
+  // });
+
+  // useEffect(() => {
+  //   if (!error) return;
+
+  //   const customError = error as ICustomError;
+
+  //   toast.error(
+  //     customError?.response?.data?.error || "Erro ao buscar desenvolvedores"
+  //   );
+  // }, [error]);
 
   useEffect(() => {
-    if (!error) return;
+    setPage(1);
+  }, [query]);
 
-    const customError = error as ICustomError;
+  useEffect(() => {
+    setIsLoading(true);
+    getDevs(page, query)
+      .then((response: IGetDesenvolvedores) => {
+        setData(response);
+      })
+      .catch((error) => {
+        const customError = error as ICustomError;
 
-    toast.error(
-      customError?.response?.data?.error || "Erro ao buscar desenvolvedores"
-    );
-  }, [error]);
+        toast.error(
+          customError?.response?.data?.error || "Erro ao buscar desenvolvedores"
+        );
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [page, query]);
+
+  useEffect(() => {
+    setQuery("");
+  }, []);
 
   return (
     <div className='p-6 max-w-full mx-auto space-y-4'>
@@ -61,7 +95,10 @@ export default function Desenvolvedores() {
       <div className='border rounded-lg p-2'>
         <TableComponent
           columns={Columns}
-          data={data?.data ?? []}
+          data={(data?.data ?? []).map((dev: IDesenvolvedor) => ({
+            ...dev,
+            nivel: dev?.nivel?.nivel,
+          }))}
           isLoading={isLoading}
         />
       </div>

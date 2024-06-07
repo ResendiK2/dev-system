03a5d2d9326d6ac006793f1be9628a61ccc19470
Gabriel Resende <gabriel@sensio.com.br>
2024-06-06 23:28:49 -0300

    refactor: Update desenvolvedores.routes.ts to include CRUD operations for developers

--- backend/src/routes/desenvolvedores.routes.ts
+++ backend/src/routes/desenvolvedores.routes.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import {
+  createDesenvolvedor,
+  deleteDesenvolvedor,
+  getDesenvolvedores,
+  updateDesenvolvedor,
+} from "../controllers/desenvolvedores.controller";
 
 const router = Router();
+
+// getDesenvolvedores,createDesenvolvedor,updateDesenvolvedor
+// deleteDesenvolvedor
+
+router.post("/desenvolvedores", createDesenvolvedor);
+router.get("/desenvolvedores", getDesenvolvedores);
+router.put("/desenvolvedores/:id", updateDesenvolvedor);
+router.delete("/desenvolvedores/:id", deleteDesenvolvedor);
 
 export default router;

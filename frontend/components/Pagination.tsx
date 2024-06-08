d93f2828fea7b294e699a97ab6475d4cd86a99e5
Gabriel Resende <gabriel@sensio.com.br>
2024-06-07 21:53:48 -0300

    refactor: Add Pagination component for handling pagination in frontend

---
+++ frontend/components/Pagination.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { IMeta } from "@/utils/types";
+import { Button } from "../components/ui/button";
+
+export function Pagination({
+  isLoading,
+  paginationData,
+  setPage,
+}: {
+  isLoading: boolean;
+  paginationData: IMeta;
+  setPage: (page: number) => void;
+}) {
+  const { current_page, last_page } = paginationData || {};
+
+  if (!current_page || !last_page) return null;
+
+  return (
+    <div className='flex justify-between items-center space-x-2 py-4'>
+      <Button
+        variant='outline'
+        size='sm'
+        onClick={() => setPage(current_page - 1)}
+        disabled={isLoading || current_page === 1}
+      >
+        Pagina anterior ({current_page ? current_page - 1 : 1})
+      </Button>
+
+      <span>
+        Página {current_page} de {last_page}
+      </span>
+
+      <Button
+        variant='outline'
+        size='sm'
+        onClick={() => setPage(current_page + 1)}
+        disabled={isLoading || last_page === current_page}
+      >
+        Proxima pagina ({current_page ? current_page + 1 : 1})
+      </Button>
+    </div>
+  );
+}

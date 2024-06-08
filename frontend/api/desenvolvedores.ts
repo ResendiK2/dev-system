48ecf82a2f2632d7099d23d3c559e4fb04167faa
Gabriel Resende <gabriel@sensio.com.br>
2024-06-08 00:08:32 -0300

    refactor: Update API functions to use IDesenvolvedorBody instead of IDesenvolvedor

--- frontend/api/desenvolvedores.ts
+++ frontend/api/desenvolvedores.ts
@@ -1,10 +1,16 @@
 // api/desenvolvedores.ts
-import { IDesenvolvedor } from "@/utils/types";
+import { IDesenvolvedorBody } from "@/utils/types";
 import axios from "axios";
 
 const API_URL = "http://localhost:3333/api/desenvolvedores";
 
-export const getDevs = async (page: number, query: string) => {
+export const getDevs = async ({
+  page,
+  query,
+}: {
+  page: number;
+  query: string;
+}) => {
   const response = await axios.get(`${API_URL}`, {
     params: {
       page,
@@ -15,13 +21,13 @@ export const getDevs = async (page: number, query: string) => {
   return response?.data || [];
 };
 
-export const createDev = async (desenvolvedor: IDesenvolvedor) => {
+export const createDev = async (desenvolvedor: IDesenvolvedorBody) => {
   const response = await axios.post(API_URL, desenvolvedor);
 
   return response.data;
 };
 
-export const updateDev = async (desenvolvedor: IDesenvolvedor) => {
+export const updateDev = async (desenvolvedor: IDesenvolvedorBody) => {
   const response = await axios.put(
     `${API_URL}/${desenvolvedor.id}`,
     desenvolvedor

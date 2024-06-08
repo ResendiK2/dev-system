5c696335be7a0eed2107d13e345fa3f491726d37
Gabriel Resende <gabriel@sensio.com.br>
2024-06-08 00:43:50 -0300

    refactor: Update SelectWithSearch component to include search functionality and dynamic data loading

--- frontend/components/SelectWithSearch.tsx
+++ frontend/components/SelectWithSearch.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import * as React from "react";
+import { useEffect, useState } from "react";
 import { Check, ChevronsUpDown } from "lucide-react";
 
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
   Command,
@@ -19,24 +18,41 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
+import { cn } from "@/lib/utils";
+import { useLevels } from "@/hooks/useLevels";
+
+import { INivel } from "@/utils/types";
 interface IItems {
   label: string;
   value: string;
 }
 
 export function SelectWithSearch({
-  items = [],
   value: initValue,
   onValueChange,
   disabled,
 }: {
-  items: IItems[];
   value: string;
   onValueChange: (value: string) => void;
   disabled?: boolean;
 }) {
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState(initValue);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(initValue);
+  const [query, setQuery] = useState<string>("");
+
+  useEffect(() => {
+    setQuery("");
+  }, []);
+
+  useEffect(() => {
+    setValue(initValue);
+  }, [initValue]);
+
+  const { data } = useLevels({ query });
+
+  const handleSearchChange = (e: string) => {
+    setQuery(e);
+  };
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -48,36 +64,48 @@ export function SelectWithSearch({
           className='w-full justify-between'
           disabled={disabled}
         >
-          {items?.find((item) => item.value === value)?.label ?? "Selecione..."}
+          {data?.data?.find((nivel: INivel) => nivel.id == +value)?.nivel ??
+            "Selecione..."}
           <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
         </Button>
       </PopoverTrigger>
 
       <PopoverContent className='w-[250px] p-0'>
         <Command>
-          <CommandInput placeholder='Buscar...' />
+          <CommandInput
+            placeholder='Buscar...'
+            value={query}
+            onValueChange={handleSearchChange}
+          />
           <CommandList>
             <CommandEmpty>Nenhum resultado encontrado</CommandEmpty>
             <CommandGroup>
-              {items.map((item) => (
-                <CommandItem
-                  key={item.value}
-                  value={item.value}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    onValueChange(currentValue);
-                    setOpen(false);
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === item.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {item.label}
-                </CommandItem>
-              ))}
+              {data?.data?.map((nivel: INivel) => {
+                const item: IItems = {
+                  label: nivel.nivel,
+                  value: String(nivel.id),
+                };
+
+                return (
+                  <CommandItem
+                    key={item.value}
+                    value={item.value}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? "" : currentValue);
+                      onValueChange(currentValue);
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === item.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {item.label}
+                  </CommandItem>
+                );
+              })}
             </CommandGroup>
           </CommandList>
         </Command>
